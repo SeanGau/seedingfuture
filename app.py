d3da166bd45450495888a08f64c9c26464910f6e
SeanGau <rrtw0627@gmail.com>
2021-03-08 22:38:19 +0800

    add moveable function

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, request, jsonify
-app = Flask(__name__)
+import flask
+import hashlib
+from os import walk
+app = flask.Flask(__name__)
+app.jinja_env.globals['GLOBAL_TITLE'] = "Seeding Future 產生器"
+
+source_img = []
+for (dirpath, dirnames, filenames) in walk("./static/source"):
+    for filename in filenames:
+        _dict = {
+            "filename": filename,
+            "hashed": hashlib.sha1(filename.encode()).hexdigest() 
+        }
+        source_img.append(_dict)
+    break
+
+print(source_img)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to Seeding Future !!</h1>"
+    return flask.render_template('index.html', sources = source_img)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
---
+++ static/index.js
@@ -0,0 +1,87 @@
+const move = new Moveable(document.body, {
+  draggable: true,
+  resizable: true,
+  rotatable: true,
+  warpable: true,
+  origin: false,
+  edge: true,
+  throttleDrag: 0,
+  throttleResize: 0,
+  throttleRotate: 5,
+});
+
+let frame = {
+  translate: [0, 0],
+};
+
+move.on("drag", e => {
+  //console.log(e);
+  $(e.target).css({ left: `${e.left}px`, top: `${e.top}px` });
+  //e.target.style.transform = e.transform;
+});
+
+move.on("resize", e => {
+  e.target.style.width = `${e.width}px`;
+  e.target.style.height = `${e.height}px`;
+});
+
+move.on("scale", ({ target, transform }) => {
+  target.style.transform = transform;
+});
+
+move.on("rotate", ({ target, transform }) => {
+  target.style.transform = transform;
+  //console.log(transform);
+});
+
+move.on("warp", ({ target, transform, }) => {
+  target.style.transform = transform;
+});
+
+
+$("#collage-area").on("click", function () {
+  move.target = null;
+  $(".target.selected").removeClass("selected");
+})
+
+$("body").on("click", "button", function() {
+  move.target = null;
+  $(".target.selected").removeClass("selected");
+})
+
+$("#collage-area").on("click", ".target", function (e) {
+  e.stopPropagation();
+  let target = this;
+  if (move.target !== target) {
+    move.target = target;
+    $(".target.selected").removeClass("selected");
+    $(target).addClass("selected");
+  }
+  else {
+    //move.resizable = !move.resizable;
+  }
+})
+
+$("#collage-tools button[name='collage-add']").on("click", function () {
+  $("#collage-source").toggleClass("show");
+})
+
+$("#collage-tools button[name='collage-clear']").on("click", function () {
+  $("#collage-area").html("");
+})
+
+$("#collage-source .source-img").on("click", function () {
+  let templateDom = $(`
+  <div class="target">
+    <img src="">
+    <button class="btn btn-danger mt-1 d-none" type="button" title="delete" name="collage-remove"><i
+            class="far fa-trash-alt"></i></button>
+  </div>
+  `);
+  $("img", templateDom).attr("src", $(this).data("src"));
+  $("#collage-area").append(templateDom);
+})
+
+$("#collage-area").on("click", ".target button[name='collage-remove']", function () {
+  $(this).parents(".target").remove();
+})
